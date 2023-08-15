ff4155631a948fa83ab4d05360de2bd72dc1c222
Tom Haborak <thaborak@gmail.com>
2023-08-14 23:19:43 -0400

    fix local docker setup

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // Connect to the MongoDB database
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/sith-scribe");
+    await mongoose.connect("mongodb://mongo:27017/sith-scribe");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error);
